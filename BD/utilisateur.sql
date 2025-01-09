c025d6a39a7b8cbbc51fce3517cf75a97d97439f
moucmoi <noatoimoi@gmail.com>
2025-01-09 10:55:30 +0100

    creation bdd

---
+++ BD/utilisateur.sql
@@ -0,0 +1,13 @@
+drop table if exists UTILISATEUR;
+
+create table UTILISATEUR(
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    nom VARCHAR(255) NOT NULL,
+    score INTEGER
+);
+
+INSERT INTO UTILISATEUR (nom, score) VALUES ('Alice', 0);
+INSERT INTO UTILISATEUR (nom, score) VALUES ('Bob', 0);
+INSERT INTO UTILISATEUR (nom, score) VALUES ('Charlie', 0);
+INSERT INTO UTILISATEUR (nom, score) VALUES ('David', 0);
+
